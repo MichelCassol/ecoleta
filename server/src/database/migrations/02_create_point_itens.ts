e448c2a57c074caad250664441d76ddf3b5f2243
Michel <michelcssoliveira@gmail.com>
2020-06-27 12:11:51 -0300

    Commit inicial

---
+++ server/src/database/migrations/02_create_point_itens.ts
@@ -0,0 +1,15 @@
+import Knex from 'knex';
+
+export async function up(knex: Knex) {
+    return knex.schema.createTable('point_items', table => {
+        table.increments('id').primary();
+        table.integer('point_id');
+        table.integer('item_id');
+        table.foreign('point_id').references('id').inTable('points');
+        table.foreign('item_id').references('id').inTable('items');
+    });
+}
+
+export async function down(knex: Knex) {
+    return knex.schema.dropTable('point_items');
+}
